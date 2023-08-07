95338d3ff184aad881c6dafe0835b2b0ab2e06bf
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-08-07 18:20:59 +0300

    Bread class created

---
+++ src/product/Bread.java
@@ -0,0 +1,20 @@
+package product;
+
+public abstract class Bread extends ProductForSale{
+
+    private String flourType;
+
+    public Bread(String type, int price, String description,String flourType) {
+        super(type, price, description);
+        this.flourType=flourType;
+    }
+
+    @Override
+    public void showDetails() {
+        String result=super.toString();
+        StringBuilder builder=new StringBuilder();
+        builder.append("Flour: "+flourType+"\n");
+        System.out.println(result+builder.toString());
+
+    }
+}
